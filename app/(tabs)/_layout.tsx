524c2a4b7acb72952df8910858c43c7c63a21254
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-12-27 02:01:01 +0100

    Feat: Tab navigation and icons for the homescreen.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
+// for the @ to be used in place of a relative path, a change was made on the tsconfig file path and app.json file
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Layout = () => {
   return (
-   <Tabs>
-    
-   </Tabs>
-  )
-}
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: { fontFamily: "mon-sb" },
+      }}
+    >
+      <Tabs.Screen
+        name="index" // name of the file 
+        options={{
+          tabBarLabel: "Explore", // name that will be displayed on the tab.
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wishlists"
+        options={{
+          tabBarLabel: "Wishlists",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+       <Tabs.Screen
+        name="trips"
+        options={{
+          tabBarLabel: "Trips",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="airbnb" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          tabBarLabel: "Inbox",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="message-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" color={color} size={size} />
+          ),
+        }}
+      />
 
-export default Layout
+    </Tabs>
+  );
+};
+
+export default Layout;
