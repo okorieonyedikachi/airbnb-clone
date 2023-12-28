b3de223922b374223f21b9e71562aee7d647813c
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-12-28 10:04:13 +0100

    Feat: ExploreHeader page and made the category onPress functional.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import React, { useState } from 'react'
+import { Link, Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
+
+// when a category is clicked on the exploreheader page, the list in listings file is reloded
 
 const Index = () => {
+  const [category, setCategory] = useState('Tiny homes')
+  // listen to category change when pressed
+  const onDataChanged = (category: string) => {
+    setCategory(category)
+  }
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/74639"}>Listing Details Page</Link>
+    <View style={{flex:1}}>
+      <Stack.Screen options={{header: () => <ExploreHeader onCategoryChanged={onDataChanged}/>}}/>
+      <Listings listings={[]} category={category}/>
     </View>
   )
 }
---
+++ components/ExploreHeader.tsx
@@ -0,0 +1,176 @@
+import { View, Text, StyleSheet, Touchable } from "react-native";
+import React, { useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import Index from "@/app/(tabs)";
+import * as Haptics from "expo-haptics"
+
+const categories = [
+  {
+    name: "Tiny homes",
+    icon: "home",
+  },
+  {
+    name: "Cabins",
+    icon: "house-siding",
+  },
+  {
+    name: "Trending",
+    icon: "local-fire-department",
+  },
+  {
+    name: "Play",
+    icon: "videogame-asset",
+  },
+  {
+    name: "City",
+    icon: "apartment",
+  },
+  {
+    name: "Beachfront",
+    icon: "beach-access",
+  },
+  {
+    name: "Countryside",
+    icon: "nature-people",
+  },
+];
+
+interface Props {
+   onCategoryChanged: (category: string) => void
+}
+
+const ExploreHeader = ({onCategoryChanged}: Props) => {
+  const scrollRef = useRef<ScrollView>(null)
+  const itemsRef = useRef<Array<TouchableOpacity | null>>([]); // Read on useRef extensively
+  const [activendex, setActiveIndex] = useState(1);
+
+
+
+  const selectCategory = (index: number) => {
+    const selected = itemsRef.current[index];
+    setActiveIndex(index)
+    
+    // Expo Haptics type error
+    // selected?.measure((x: number) => {
+    //   scrollRef.current?.scrollTo({ x: x - 16, y: 0, animated: true });
+    // });
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    onCategoryChanged(categories[index].name) // sorta look out for change in the categories
+  };
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <View style={styles.actionRow}>
+          <Link href={"/(modals)/booking"} asChild>
+            <TouchableOpacity style={styles.searchBtn}>
+              <Ionicons name="search" size={24} />
+              <View>
+                <Text style={{ fontFamily: "mon-sb" }}>Where to</Text>
+                <Text style={{ fontFamily: "mon", color: Colors.grey }}>
+                  Anywhere . Anyweek
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </Link>
+
+          <TouchableOpacity style={styles.filterBtn}>
+            <Ionicons name="options-outline" size={24} />
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView
+         ref={scrollRef}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            alignItems: "center",
+            gap: 30,
+            paddingHorizontal: 10,
+          }}
+        >
+          {categories.map((item, index) => (
+            <TouchableOpacity
+              onPress={() => selectCategory(index)}
+              key={index}
+              ref={(el) => (itemsRef.current[index] = el)}
+              style={activendex === index ? styles.categoriesBtnActive : styles.categoriesBtn} //
+            >
+              <MaterialIcons size={24} name={item.icon as any} color={activendex === index ? "#000" : Colors.grey}/>
+              <Text style={activendex === index ? styles.categoryTextActive: styles.categoryText}>{item.name}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    height: 130,
+  },
+  actionRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 24,
+    paddingBottom: 16,
+    gap: 10,
+  },
+  filterBtn: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    borderRadius: 24,
+  },
+  searchBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    borderColor: "#c2c2c2",
+    borderWidth: StyleSheet.hairlineWidth,
+    width: 280,
+    padding: 14,
+    borderRadius: 30,
+    backgroundColor: "#fff",
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.12,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  categoryText: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+    color: Colors.grey,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+    color: "#000",
+  },
+  categoriesBtn: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 8,
+  },
+  categoriesBtnActive: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomColor: "#000",
+    borderBottomWidth: 2,
+    paddingBottom: 8,
+  },
+});
+
+export default ExploreHeader;
